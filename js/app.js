10d427e9c5c5bf303ebeed6634b0650d572030f0
disim <magdalenaduvnjak@gmail.com>
2016-10-15 15:35:00 +0200

    Add fetchUser, render, shouldRender functions

--- js/app.js
+++ js/app.js
@@ -9,23 +9,60 @@ $(function(){
 /**
  * gets a random user from randomuser.me
  */
+ var recieved = false;
+ var currentUser = {};
+
 function fetchUser() {
     // TODO: implement
+    $.getJSON('https://randomuser.me/api/', function(data) {
+        currentUser = data.results[0];
+        //console.log(currentUser.nat);--------> delete after testing
+    });
 }
 
 /**
  * creates and inserts the popup into the DOM
  */
 function render() {
-    // TODO: implement
+    $('body').append(
+        "<div class='follow'>"
+            + "<div class='popup-image'>"
+                + "<img src='" + currentUser.picture.large + "'>"
+            +  "</div>"
+            + "<div class ='popup-text'>"
+                + "<div class = 'popup-close'> X </div>"
+                + "<div class = 'popup-article'>"
+                    + "<b>Hi there!</b>"
+                    + "<br /> "
+                    + "For any questions contact me via "
+                         + "<a href='mailto:" + currentUser.email + "'>email</a>"
+                + "</div>"
+            + "</div>"
+        + "</div>");
+
+    if(currentUser.gender === "male"){
+        $(".follow").css("background", "lightblue");
+    } else {
+        $(".follow").css("background", "lightcoral");
+    }
+
+    $(".popup-close").click(function () {
+        $(".follow").remove();
+    });
 }
 
 /**
  * checks if all conditions are met to render the popup
- * @return Boolean 
+ * @return Boolean
  */
-function shouldRender() {
+function shouldRender() {//&& currentUser.nat==="FR"-------> add after testing
     // TODO: implement
+    if(((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) >= 0.65){
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 /**
